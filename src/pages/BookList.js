ad0986eca000fc0f5ae9e6ba42eadfa293647e30
Reward Khe <rewardkhe@outlook.com>
2023-03-23 07:26:33 +1100

    buggo

--- src/pages/BookList.js
+++ src/pages/BookList.js
@@ -4,7 +4,7 @@ export function BookList() {
     return (
     <>
     <h1> BookList</h1>
-    <Link to = "/books/1"> Book 1 </Link> <br> </br>
+    <Link to = "/books/1"> Book 1 </Link> 
     <Link to = "/books/2"> Book 2 </Link>
     </>
     )    
